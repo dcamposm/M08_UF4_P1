0f020ddd9daec0f9cf4fdc888a827ff8ef2ca0bc
DieCM <didaccampos@gmail.com>
2018-09-26 19:45:39 +0200

    Primer commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        class gos{
+     
+            // Atributos
+            public $raza;
+            public $color;
+            public $tamany;
+
+            // Constructor
+            public function __construct($raza, $color, $tamany){
+                $this->raza = $raza;
+                $this->color = $color;
+                $this->tamany = $tamany;
+            }
+            
+        }
+        ?>
+    </body>
+</html>
